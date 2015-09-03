b1ad9928c74569b66db6d4c822cb1a3e8132ba83
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-09-03 02:08:03 -0300

    env

--- config/env/test.js
+++ config/env/test.js
@@ -8,7 +8,7 @@ module.exports = {
 	travisJobNumber : process.env.TRAVIS_JOB_NUMBER,
 	travisBuild : process.env.TRAVIS_BUILD_NUMBER,
 	clientID : process.env.CLIENT_ID,
-	clientSecret :process.env.CLIENT_SECRETM,
+	clientSecret :process.env.CLIENT_SECRET,
 	seleniumUser : process.env.SELENIUM_USER,
 	seleniumUserPassword : process.env.SELENIUM_USER_PASSWORD
 
