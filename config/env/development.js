65152d60366b679d96554013d87e0c050048cef2
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-09-04 14:27:03 -0300

    config, passport and deploy for travis CI

--- config/env/development.js
+++ config/env/development.js
@@ -1,13 +1,13 @@
-module.exports = {
+// module.exports = {
 
-	env:'developement',
-	db:'mongodb://localhost/contatooh',
-	clientID : process.env.CLIENT_ID,
-	clientSecret : process.env.CLIENT_SECRET,
-	seleniumUser : process.env.SELENIUM_USER,
-	seleniumUserPassword : process.env.SELENIUM_USER_PASSWORD,
-	port:3000,
-	address:'localhost',
-	domain:'localhost:3000'
+// 	env:'developement',
+// 	db:'mongodb://localhost/contatooh',
+// 	clientID : process.env.CLIENT_ID,
+// 	clientSecret : process.env.CLIENT_SECRET,
+// 	seleniumUser : process.env.SELENIUM_USER,
+// 	seleniumUserPassword : process.env.SELENIUM_USER_PASSWORD,
+// 	port:3000,
+// 	address:'localhost',
+// 	domain:'localhost'
 
-}
+// }
