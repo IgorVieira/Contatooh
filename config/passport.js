d54e6dded8486a13b8f26d864829377b8d6936d0
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-09-04 14:01:03 -0300

    config, passport and deploy for travis CI

--- config/passport.js
+++ config/passport.js
@@ -1,38 +1,46 @@
-var passport = require('passport');
+var passport =  require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
+//var findOrCreate = require('mongoose-findorcreate');
 var mongoose = require('mongoose');
 var config = require('./config')();
-module.exports = function() {
-  var githubCallback = 'http://' + config.domain + '/auth/github/callback';
-  
-  var Usuario = mongoose.model('Usuario');
 
-    passport.use(new GitHubStrategy({
-    clientID: config.clientID,
-    clientSecret: config.clientSecret,
-    callbackURL: githubCallback
-  }, function(accessToken, refreshToken, profile, done) {
+
+module.exports =  function(){
+
+  //var githubCallback = "http://"+config.domain+':'+config.port'+/auth/github/callback';  
+  var githubCallback = "http://"+config.domain+'/auth/github/callback';  
+  var Usuario =  mongoose.model('Usuario');
+  
+  
+
+  
+  passport.use(new GitHubStrategy({
+      clientID:config.clientID,
+      clientSecret:config.clientSecret,
+      callbackURL:githubCallback
+  }
+  ,function(accessToken, refreshToken, profile, done){
     Usuario.findOrCreate(
-      { "login" : profile.username}, 
-      { "nome" : profile.username},  
-      function(erro, usuario) {
-        if(erro) {
-          console.log(erro);
-          return done(erro);
-        } 
-        return done(null, usuario);
-      }
-    );
+        { "login" : profile.username},
+        { "nome" : profile.username},
+        function(erro, usuario) {
+          if(erro) {
+            return done(erro);
+          }
+          return done(null, usuario);
+        }
+      );
   }));
 
-  passport.serializeUser(function(usuario, done) {
+  passport.serializeUser(function(usuario, done){
     done(null, usuario._id);
   });
-   
-  passport.deserializeUser(function(id, done) {
+
+  passport.deserializeUser(function(id, done){
     Usuario.findById(id).exec()
-    .then(function(usuario) {
-      done(null, usuario);  
+    .then(function(usuario){
+        done(null,usuario);
     });
   });
+
 };
