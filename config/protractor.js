94115837ae28d0ecabd50b4713285252b05b8d72
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-09-04 11:58:06 -0300

    try fixed auth-2

--- config/protractor.js
+++ config/protractor.js
@@ -11,7 +11,7 @@ exports.config = {
 
   specs: ['../test/e2e/**/*Spec.js'],
   onPrepare: function() {
-    browser.driver.get('http://localhost:3000').then(function() {
+    browser.driver.get('http://localhost:3000/#/auth').then(function() {
       browser.driver.findElement(by.id('entrar')).click()
       browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
       browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
